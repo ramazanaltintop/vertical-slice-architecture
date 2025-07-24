4b044baccd4f6473d111629e0f63976c3b2db09d
Ramazan Altintop <112395498+ramazanaltintop@users.noreply.github.com>
2025-07-24 15:51:49 +0300

    feat: add OpenAPI and Scalar documentation setup

--- src/VerticalSliceArchitecture.WebAPI/Program.cs
+++ src/VerticalSliceArchitecture.WebAPI/Program.cs
@@ -1,6 +1,15 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 
 app.Run();
