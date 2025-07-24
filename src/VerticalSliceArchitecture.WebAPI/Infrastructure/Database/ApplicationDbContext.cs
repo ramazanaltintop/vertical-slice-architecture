b6e1655eeb0ea8d15ce293f100444f3ce11cb425
Ramazan Altintop <112395498+ramazanaltintop@users.noreply.github.com>
2025-07-24 17:55:22 +0300

    feat: override SaveChangesAsync for audit functionality

--- src/VerticalSliceArchitecture.WebAPI/Infrastructure/Database/ApplicationDbContext.cs
+++ src/VerticalSliceArchitecture.WebAPI/Infrastructure/Database/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VerticalSliceArchitecture.WebAPI.Domain.Bases;
 using VerticalSliceArchitecture.WebAPI.Domain.Entities;
 
 namespace VerticalSliceArchitecture.WebAPI.Infrastructure.Database;
@@ -11,5 +12,32 @@ public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<Entity>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(p => p.CreatedAt).CurrentValue = DateTimeOffset.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                if (entry.Property(p => p.IsDeleted).CurrentValue == true)
+                {
+                    entry.Property(p => p.DeletedAt).CurrentValue = DateTimeOffset.Now;
+                }
+                else
+                {
+                    entry.Property(p => p.UpdatedAt).CurrentValue = DateTimeOffset.Now;
+                }
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 }
