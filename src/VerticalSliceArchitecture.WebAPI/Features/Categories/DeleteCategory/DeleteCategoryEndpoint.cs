e36aec09de7819adde0a7158398e516b0a053a69
Ramazan Altintop <112395498+ramazanaltintop@users.noreply.github.com>
2025-07-25 16:38:00 +0300

    feat: add DeleteCategory vertical slice

---
+++ src/VerticalSliceArchitecture.WebAPI/Features/Categories/DeleteCategory/DeleteCategoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using VerticalSliceArchitecture.WebAPI.Common.Abstractions;
+using VerticalSliceArchitecture.WebAPI.Common.Constants;
+
+namespace VerticalSliceArchitecture.WebAPI.Features.Categories.DeleteCategory;
+
+public class DeleteCategoryEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapDelete(ApiRoutes.Categories.Delete, async (
+            Guid id,
+            IDeleteCategoryHandler handler,
+            CancellationToken cancellationToken) =>
+        {
+            bool result = await handler.HandleAsync(id, cancellationToken);
+
+            if (!result)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(new { Message = "Category was successfully deleted" });
+        })
+        .WithTags("Categories")
+        .WithName("DeleteCategory")
+        .WithSummary("Delete category")
+        .WithDescription("Deletes the category identified by the given ID.");
+    }
+}
---
+++ src/VerticalSliceArchitecture.WebAPI/Features/Categories/DeleteCategory/DeleteCategoryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VerticalSliceArchitecture.WebAPI.Infrastructure.Database;
+
+namespace VerticalSliceArchitecture.WebAPI.Features.Categories.DeleteCategory;
+
+public interface IDeleteCategoryHandler
+{
+    Task<bool> HandleAsync(Guid id, CancellationToken cancellationToken);
+}
+
+public class DeleteCategoryHandler(
+    ApplicationDbContext context) : IDeleteCategoryHandler
+{
+    public async Task<bool> HandleAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var category = await context.Categories
+            .Where(c => c.IsDeleted == false)
+            .FirstOrDefaultAsync(c => c.Id == id);
+
+        if (category is null)
+            return false;
+
+        category.IsDeleted = true;
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
