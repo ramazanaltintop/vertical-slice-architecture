3ebdc844b3d6b083900ea6906ac820a4bd93463a
Ramazan Altintop <112395498+ramazanaltintop@users.noreply.github.com>
2025-07-25 15:18:30 +0300

    feat: add PatchCategoryName vertical slice

---
+++ src/VerticalSliceArchitecture.WebAPI/Features/Categories/PatchCategoryName/PatchCategoryNameValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace VerticalSliceArchitecture.WebAPI.Features.Categories.PatchCategoryName;
+
+public sealed class PatchCategoryNameValidator : AbstractValidator<PatchCategoryNameCommand>
+{
+    public PatchCategoryNameValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Category name cannot be empty")
+            .NotNull().WithMessage("Category name cannot be empty")
+            .MinimumLength(2).WithMessage("Category name must be at least 2 characters");
+    }
+}
